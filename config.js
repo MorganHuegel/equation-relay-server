d4e4d9c8074326c10f654ed8aed70b8608422bca
MorganHuegel <mhue2910@gmail.com>
2019-01-20 10:29:34 -0500

    moved development database to cloud mlab

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 /* eslint-disable strict */
 
 module.exports = {
-  DB_URL: process.env.DB_URL || 'mongodb://localhost/equation-relay',
+  //DB_URL: process.env.DB_URL || 'mongodb://localhost/equation-relay',
+  DB_URL: process.env.DB_URL || 'mongodb://development:baseball1@ds159574.mlab.com:59574/equation-relay-development',
   PORT: process.env.PORT || 8080,
   JWT_SECRET: process.env.JWT_SECRET || 'PURPLE-FOO-BAR'
 };
