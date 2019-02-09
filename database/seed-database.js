82f720ef2ab806675f42acba4b16fa4c04727b8f
MorganHuegel <mhue2910@gmail.com>
2019-02-09 10:29:55 -0500

    updated dummy data for GameSession

--- database/seed-database.js
+++ database/seed-database.js
@@ -6,12 +6,14 @@ const { DB_URL } = require('../config');
 const User = require('../models/user');
 const Game = require('../models/game');
 const Question = require('../models/question');
+const GameSession = require('../models/gameSession');
 
 const DummyUsers = require('./dummy-users.json');
 const DummyGames = require('./dummy-games.json');
 const DummyQuestions = require('./dummy-questions.json');
+const DummyGameSessions = require('./dummy-game-sessions.json');
 
-mongoose.connect(DB_URL, error => {
+mongoose.connect(DB_URL,  { useNewUrlParser: true }, error => {
   if (error) {
     console.error('ERROR: ', error);
   }
@@ -25,6 +27,10 @@ mongoose.connect(DB_URL, error => {
     .then(dbResponse => {
       console.log(dbResponse);
       return Question.insertMany(DummyQuestions);
+    })
+    .then(dbResponse => {
+      console.log(dbResponse);
+      return GameSession.insertMany(DummyGameSessions);
     })
     .then(dbResponse => {
       console.log(dbResponse);
