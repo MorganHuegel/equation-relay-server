0abbd65294e593fb62a82cbf2c8b023115132744
MorganHuegel <mhue2910@gmail.com>
2019-03-04 11:25:08 -0500

    added Wrong Answer socket event

--- sockets/wrongAnswerEvent.js
+++ sockets/wrongAnswerEvent.js
@@ -1,7 +1,26 @@
 const GameSession = require('../models/gameSession');
 
 exports.wrongAnswerEvent = function (socket, sessionCode, teamId, playerId) {
-  console.log('PLAYER ID', playerId)
-  console.log('TEAM ID', teamId)
-  console.log('WRONG< YO!')
+  return GameSession.findOne({sessionCode})
+    .then(gameData => {
+      const team = gameData = gameData.teamList.find(team => team._id.toString() === teamId);
+
+      team.players.forEach(player => {
+        if (player._id.toString() === playerId) {
+          player.alreadyGuessed = true;
+        }
+      });
+      
+      return GameSession.findOneAndUpdate({sessionCode, 'teamList._id': teamId}, {$set: {'teamList.$': team}}, {new: true});
+    })
+    .then(updatedGameData => {
+      if (!updatedGameData) {
+        return Promise.reject(new Error('Game didnt update for wrong answer, yo!'));
+      }
+      socket.nsp.to(sessionCode).emit('wrongAnswer', updatedGameData);
+    })
+    .catch(err => {
+      console.log('ERROR: ',err);
+      socket.nsp.to(socket.id).emit('error', err.message);
+    });
 };
